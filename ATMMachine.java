adae5b6dc498317a57cefbc027b7b7b0e32e017d
rikshith bommena <161211708+rikshithbommena@users.noreply.github.com>
2024-07-18 19:02:40 +0530

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,93 @@
+
+import java.util.*;
+
+class ATM{
+    
+    float Balance;
+    int PIN = 7416;
+
+    public void checkpin(){
+        System.out.println("Enter your pin: ");
+        Scanner sc = new Scanner(System.in);
+        int enteredpin = sc.nextInt();
+        if(enteredpin == PIN){
+            menu();
+        }
+        else{
+            System.out.println("Enter a valid pin");
+        }
+
+    }
+
+
+
+    public void menu(){
+          System.out.println("Enter your choice: ");
+          System.out.println("1. Check A/C Balance");
+          System.out.println("2. Withdraw Money");
+          System.out.println("3. Deposit money");
+          System.out.println("4. EXIT");
+
+          Scanner sc = new Scanner(System.in);
+          int opt = sc.nextInt();
+
+          if(opt == 1){
+            checkBalanace();
+          }
+          else if(opt == 2){
+            WithdrawMoney();
+          }
+          else if(opt == 3){
+            DepositMoney();
+          }
+          else if(opt == 4){
+            return; 
+          }
+          else{
+            System.out.println("Enter a valid choice");
+          }
+    }
+
+    public void checkBalanace(){
+        System.out.println("Balance:" + Balance);
+        menu();
+    }
+
+    public void WithdrawMoney(){
+        System.out.println("Enter Amount to Withdraw:");
+        Scanner sc = new Scanner(System.in);
+        float amount = sc.nextFloat();
+        if(amount>Balance){
+            System.out.println("Insufficient Balance");
+    }
+    else{
+         Balance = Balance - amount;
+         System.out.println("Money Withdrawed Successfully");
+    }
+    menu();
+
+    }
+
+
+    public void DepositMoney(){
+        System.out.println("Enter the Amount: ");
+        Scanner sc = new Scanner(System.in);
+        float amount = sc.nextFloat();
+        Balance = Balance + amount;
+            System.out.println(" Money Deposited Successfully");
+            menu();
+        }
+    }
+
+
+
+
+public class ATMMachine{
+    public static void main(String[] args) {
+
+        ATM obj = new ATM();
+        obj.checkpin();
+
+        
+    }
+}
